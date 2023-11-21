d42cd44bf22630430edab1f7deedfb7f5e83d867
ruansilan <ruansilan@szkingdom.com>
2023-11-21 16:29:37 +0800

    添加配置uni-ui

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-  <view class="index">index</view>
+  <uni-calendar
+	:insert="true"
+	:lunar="true" 
+	:start-date="'2019-3-2'"
+	:end-date="'2019-5-20'"
+	/>
 </template>
 
 <style lang="scss">
